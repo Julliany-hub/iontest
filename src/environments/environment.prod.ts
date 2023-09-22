3ed6ad1c69fddf0de17e02f590475751b2b7d1fc
Julliany-hub <jullianytecnico@gmail.com>
2023-09-22 14:03:14 -0300

    commit

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
   production: true
 };
+const firebaseConfig = {
+  apiKey: "AIzaSyCk9CTpQXvOq4bb_m_MG2IVaPOf71PT7vY",
+  authDomain: "ionictest-dc180.firebaseapp.com",
+  projectId: "ionictest-dc180",
+  storageBucket: "ionictest-dc180.appspot.com",
+  messagingSenderId: "625666752683",
+  appId: "1:625666752683:web:0271f028a8e1ec7e6df7ee",
+  measurementId: "G-F5193S5NCB"
+};
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,7 +5,15 @@
 export const environment = {
   production: false
 };
-
+const firebaseConfig = {
+  apiKey: "AIzaSyCk9CTpQXvOq4bb_m_MG2IVaPOf71PT7vY",
+  authDomain: "ionictest-dc180.firebaseapp.com",
+  projectId: "ionictest-dc180",
+  storageBucket: "ionictest-dc180.appspot.com",
+  messagingSenderId: "625666752683",
+  appId: "1:625666752683:web:0271f028a8e1ec7e6df7ee",
+  measurementId: "G-F5193S5NCB"
+};
 /*
  * For easier debugging in development mode, you can import the following file
  * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
